1aa80f86fc64bcc0c3086d2c4267e5a9e7cbd66c
Fede <fedegonzalez_84@hotmail.com>
2018-11-10 11:17:20 -0300

    Otro commit en alternativa

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log('Hola Javascript');
-console.log('Agrego una segunda linea');
+console.log('Agrego una segunda linea y pruebo mergear');
 console.log('Linea perteneciente al tercer commit');
