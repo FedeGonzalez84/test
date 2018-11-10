8f0643b4691fd8910e84c77008cd0578c66d9810
Federico González <fedegonzalez_84@hotmail.com>
2018-11-10 11:23:44 -0300

    Se modifico desde github

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log('Hola Javascript');
 console.log('Agrego una segunda linea y pruebo mergear');
 console.log('Linea alternativa 2');
 console.log('Linea perteneciente al tercer commit');
+console.log('Linea agregada desde GitHub')
